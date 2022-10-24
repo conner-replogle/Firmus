d763d36ce8be314bcef11682d8d8d16c0689e5d5
conner-replogle <connerlreplogle@gmail.com>
2022-10-24 15:46:25 -0500

    progress

--- firmus_daemon/src/main.rs
+++ firmus_daemon/src/main.rs
@@ -1,111 +1,135 @@
 mod process;
 mod stream_wrapper;
-use std::{env, task::Poll, net::{TcpListener, SocketAddr, TcpStream}, thread, sync::{Mutex, Arc, RwLock, PoisonError}, str::FromStr, collections::HashMap};
 use arc_swap::ArcSwap;
 use clap::Parser;
-use firmus_lib::communication::{ConnectionType, instructor::{self, Command}};
+use firmus_lib::communication::{
+    instructor::{self, Command},
+    ConnectionType,
+};
 use process::Process;
-use serde::{Serialize, Serializer, Deserialize, Deserializer};
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
+use std::{
+    collections::HashMap,
+    env,
+    net::{SocketAddr, TcpListener, TcpStream},
+    str::FromStr,
+    sync::{Arc, Mutex, PoisonError, RwLock},
+    task::Poll,
+    thread, io::Read,
+};
 use stream_wrapper::Stream;
 
 use std::sync::mpsc;
 
-type ThreadSafeProcess = Arc<Mutex<HashMap<String,Process>>>;
+type ThreadSafeProcess = Arc<Mutex<HashMap<u32, Arc<Mutex<Process>>>>>;
 
-#[derive(thiserror::Error,Debug)]
-enum FirmusError{
+#[derive(thiserror::Error, Debug)]
+enum FirmusError {
     #[error("TcpListener fucked up with this error {0}")]
     Disconnect(#[from] std::io::Error),
     #[error("Failed to parse a message: {0}")]
     FailedParsingCommunication(#[from] bincode::Error),
-
-
-}
-#[derive(PartialOrd, Ord, PartialEq, Eq, Clone, Debug)]
-struct Procceses(Vec<Process>);
-
-impl Serialize for Procceses {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        // If you implement `Deref`, then you don't need to add `.0`
-        let s = format!("{}", self.0.format(SERIALIZE_FORMAT));
-        serializer.serialize_str(&s)
-    }
 }
 
-impl<'de> Deserialize<'de> for Procceses {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        let s = String::deserialize(deserializer)?;
-        
-    }
-}
-
-fn main() -> Result<(),FirmusError>{
+fn main() -> Result<(), FirmusError> {
     let addr: SocketAddr = SocketAddr::from_str("127.0.0.1:8888").unwrap();
     let listener = TcpListener::bind(addr).unwrap();
 
     let mut thread_safe_processes: ThreadSafeProcess = Arc::from(Mutex::from(HashMap::new()));
 
-
     for stream in listener.incoming() {
-        
         match stream {
             Err(_) => println!("listen error"),
             Ok(stream) => {
                 let mut stream = Stream::from(stream);
-                println!("connection from {} to {}",
-                        stream.inner.peer_addr().unwrap(),
-                        stream.inner.local_addr().unwrap());
+                println!(
+                    "connection from {} to {}",
+                    stream.inner.peer_addr().unwrap(),
+                    stream.inner.local_addr().unwrap()
+                );
                 let connection_type: ConnectionType = stream.read()?;
-                match connection_type{
+                match connection_type {
                     ConnectionType::Program => {
                         let thread_safe_processes = thread_safe_processes.clone();
-                        thread::spawn(move ||{
-                            handle_program(stream,thread_safe_processes).unwrap();
+                        thread::spawn(move || {
+                            handle_program(stream, thread_safe_processes).unwrap();
                         });
-                    },
+                    }
                     ConnectionType::Instructor => {
                         let thread_safe_processes = thread_safe_processes.clone();
-                        thread::spawn(move ||{
-                            handle_instructor(stream,thread_safe_processes).unwrap();
+                        thread::spawn(move || {
+                            handle_instructor(stream, thread_safe_processes).unwrap();
                         });
-
-                    },
+                    }
                 }
             }
         }
     }
     Ok(())
 }
-fn handle_program(mut stream: Stream,thread_safe_processes:ThreadSafeProcess) -> Result<(),FirmusError>{
+fn handle_program(
+    mut stream: Stream,
+    thread_safe_processes: ThreadSafeProcess,
+) -> Result<(), FirmusError> {
     let mut processes = thread_safe_processes.lock().unwrap();
-    let id: String = stream.read()?;
-    if let Some(process) = processes.get_mut(&id){
+    let id: u32 = stream.read()?;
+    if let Some(process) = processes.get_mut(&id) {
+        let mut process = process.lock().unwrap();
         process.stream = Some(stream);
-
     }
     Ok(())
 }
-fn handle_instructor(mut stream: Stream,thread_safe_processes:ThreadSafeProcess) -> Result<(),FirmusError>{
+fn handle_instructor(
+    mut stream: Stream,
+    thread_safe_processes: ThreadSafeProcess,
+) -> Result<(), FirmusError> {
     let command: Command = stream.read()?;
-    match command{
+    match command {
         Command::Start(config) => {
-            
-        },
-        Command::Stop(id) => {
-            
-        },
-        Command::List =>{
+            start_program(stream, thread_safe_processes, config);
+        }
+        Command::Stop(id) => {}
+        Command::List => {
             let processes = thread_safe_processes.lock().unwrap();
             //stream.write(processes.values());
-
-
-        },
+        }
     }
     Ok(())
+}
+
+fn start_program(
+    stream: Stream,
+    thread_safe_processes: ThreadSafeProcess,
+    config: firmus_lib::config::Config,
+){
+    let mut process = Process::from(config);
+    if let Ok(pid) = process.start(){
+        let mut proccesses = thread_safe_processes.lock().unwrap();
+        proccesses.insert(pid,Arc::from(Mutex::from(process)));
+        let process = proccesses.get_mut(&pid).unwrap();
+        let mut p = process.lock().unwrap();
+        let child = p.child.as_mut().unwrap();
+        let mut stdout = child.stdout.take().unwrap();
+        let mut stderr = child.stderr.take().unwrap();
+        drop(child);drop(p);drop(process);drop(proccesses);
+        thread::scope(|s|{
+            s.spawn(||{
+                loop{
+                    let mut buf = Vec::new();
+                    if stdout.read(&mut buf).unwrap() > 0{
+                        
+                    }
+                    
+                }
+            });
+
+        });
+        loop{
+            
+        }
+
+
+
+    }
+    
 }
--- firmus_daemon/src/process.rs
+++ firmus_daemon/src/process.rs
@@ -1,30 +1,51 @@
+use std::{process::{Command, Child, Stdio}, thread, io};
+
+use firmus_lib::config::Config;
+
 use crate::stream_wrapper::Stream;
-#[derive(serde::Serialize,serde::Deserialize)]
-pub struct ProcessStatus{
+#[derive(serde::Serialize, serde::Deserialize)]
+pub struct ProcessStatus {
     id: String,
     ipc_online: bool,
     process_online: bool,
     pid: String,
-
-
 }
 
-pub struct Process{
-    pub handle: Handle
-    pub stream: Option<Stream>
+#[derive(thiserror::Error, Debug)]
+pub enum ProcessError {
+    #[error("Process failed to start: {0}")]
+    FailedToStart(#[from] std::io::Error),
 }
-impl Process{
-    pub fn start({
-        
-    })
-    pub fn into_status(&self)-> ProcessStatus{
-        ProcessStatus{
-            id: "".to_string()  ,
+
+pub struct Process {
+    pub config: Config,
+    pub child: Option<Child>,
+    pub stream: Option<Stream>,
+}
+impl Process {
+    pub fn from(config: Config) -> Self {
+        Self {
+            stream: None,
+            config,
+            child: None
+        }
+    }
+    pub fn start(&mut self) -> Result<u32,ProcessError> {
+        let mut command = Command::new(&self.config.run_command);
+        command.stdout(Stdio::piped());
+        command.stderr(Stdio::piped());
+        let child = command.spawn()?;
+        let pid = child.id();
+        self.child = Some(child);
+
+        Ok(pid)
+    }
+    pub fn into_status(&self) -> ProcessStatus {
+        ProcessStatus {
+            id: "".to_string(),
             ipc_online: self.stream.is_some(),
             process_online: self.stream.is_some(),
             pid: "".to_string(),
         }
-        
-
     }
 }
